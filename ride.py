a441195910246f86364a9619c6c16b95099e79f2
mattymecks <mcmecoli@gmail.com>
2018-06-29 15:04:15 -0400

    Done.

--- ride.py
+++ ride.py
@@ -1,2 +1,89 @@
 class Ride:
-    pass
+
+    experience = "awesome"
+    tagline = "At least we're not Lyft"
+    easter_egg = "This is an easter egg."
+    goose = "Duck, Duck, motherfucker"
+
+    # def __init__(self, ride_id, driver_id, number_of_passengers, origin, destination, car_info, price):
+    #     '''This class describes the pertient information
+    #         for each ride take with fuber'''
+    #     self._ride_id = ride_id
+    #     #going to use this to connect our instance here with the likely generalized
+    #     #method of iding rides in the central database of this info
+    #     #i.e. this is going to match our storage info with our analytics info
+    #     self._driver_id = driver_id
+    #     #likely going to be a string, possibly an ID
+    #     self._number_of_passengers =
+    #     #an integer value
+    #     self._origin =
+    #     #gps coordinate perhaps?
+    #     self._destination =
+    #     #another gps coordinate? May use the two to calc distance and more
+    #     self._car_info = {}
+    #     #probably points to a dictionary with info on car
+    #     self._price =
+    #     #float, probably
+    #     return !!!!!
+    #
+    # #getter and setter for ride_id
+    # @property
+    # def ride_id(self):
+    #     return self._ride_id
+    #
+    # @ride_id.setter
+    # def ride_id(self, ride_id):
+    #     self._ride_id = ride_id
+    #
+    # #getter and setter for driver_id
+    # @property
+    # def driver_id(self):
+    #     return self._drive_id
+    #
+    # @driver_id.setter
+    # def driver_id(self, driver_id):
+    #     self._driver_id = driver_id
+    #
+    # #getter and setter for number_of_passengers
+    # @property
+    # def number_of_passengers(self):
+    #     return self._number_of_passengers
+    #
+    # @number_of_passengers.setter
+    # def number_of_passengers(self, number_of_passengers):
+    #     self._number_of_passengers = number_of_passengers
+    #
+    # #getter and setter for ride_id
+    # @property
+    # def ride_id(self):
+    #     return self._ride_id
+    #
+    # @ride_id.setter
+    # def ride_id(self, ride_id):
+    #     self._ride_id = ride_id
+    #
+    # #getter and setter for ride_id
+    # @property
+    # def ride_id(self):
+    #     return self._ride_id
+    #
+    # @ride_id.setter
+    # def ride_id(self, ride_id):
+    #     self._ride_id = ride_id
+    #
+    # #getter and setter for ride_id
+    # @property
+    # def ride_id(self):
+    #     return self._ride_id
+    #
+    # @ride_id.setter
+    # def ride_id(self, ride_id):
+    #     self._ride_id = ride_id
+    #
+    #
+    #
+    #
+
+
+
+#give me some space, Python
